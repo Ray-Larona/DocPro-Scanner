b804360db14796c34f361a2440c62925ac2a79a5
Ray-Larona <olaremato@gmail.com>
2026-08-10 13:14:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -764,62 +764,6 @@ function(){
    
 });
 
-/* ==========================
-   REPLACE IMAGE
-========================== */
-
-
-document
-.getElementById("replaceBtn")
-.addEventListener(
-"click",
-function(){
-
-
-    if(currentPreviewIndex === null){
-
-        return;
-
-    }
-
-
-
-    capturePhoto();
-
-
-
-    capturedImages.splice(
-
-        currentPreviewIndex,
-
-        1,
-
-        capturedImages[
-            capturedImages.length - 1
-        ]
-
-    );
-
-
-
-    capturedImages.pop();
-
-
-
-    updateThumbnails();
-
-
-
-    document
-    .getElementById(
-        "previewImage"
-    )
-    .src =
-    capturedImages[currentPreviewIndex];
-
-
-
-});
 /* ==========================================
    DOCPRO SCANNER V2
    APP.JS PART 3/3
